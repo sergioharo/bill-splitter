a6c914bbf55a3141f442008aaadb32866e6ac6a3
sergioharo <sergio.haro.jr@gmail.com>
2012-10-29 14:07:32 -0700

    Changing layout of all people template, removed placeholder div, moved footer out of container, and added scroll on load to hide address bar on mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(function()
 {
-	var app = new sh.router({ container: $("#placeholder")});
+	var app = new sh.router({ container: $("#container")});
 	Backbone.history.start();
 	/* Backbone.history.start({
 		pushState: true,
 		root: "/~Sergio/bill-splitter/"
 	});	*/
+
+	// Hide the address bar!
+	setTimeout(function(){
+	    //window.scrollTo(0);
+	}, 0);
 });
